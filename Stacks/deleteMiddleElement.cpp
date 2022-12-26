0a417f71d213057c70ebbbd4a47863e276a95f48
maanindermaan <113203123+maanindermaan@users.noreply.github.com>
2022-12-27 01:00:21 +0530

    Create deleteMiddleElement.cpp

---
+++ Stacks/deleteMiddleElement.cpp
@@ -0,0 +1,24 @@
+// // Deleting middle element from stack
+// #include <bits/stdc++.h> 
+
+// void solve(stack<int>&inputStack , int count , int size){
+// //     Base Case
+//     if(count == size/2){
+//         inputStack.pop();
+//         return;
+//     }
+    
+// //     Isko bacha ke side mein rakhlo uper wale !
+//     int num = inputStack.top();
+//     inputStack.pop();
+    
+// //     Recursive call
+//     solve(inputStack , count+1 , size);
+    
+//     inputStack.push(num);
+// }
+
+// void deleteMiddle(stack<int>&inputStack, int N){
+// 	int count = 0;
+//     solve(inputStack ,count , N);
+// }
